f0bf6f7201aef2d1f06ac4b27ef245d42c157c8b
theman49 <cakdeny49@gmail.com>
2023-10-12 22:54:09 +0700

    add class "transition-1"

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -9,13 +9,13 @@ export default function ToggleTheme() {
     return (
         <div id="toggleTheme" className="flex gap-2 bg-gray-2 rounded-full py-1 px-2 opacity-80"  onClick={toggleTheme}>
             <div className={`
-                    flex justify-center items-center p-1 rounded-full 
+                    flex justify-center items-center p-1 rounded-full transition-1 
                     ${$theme !== 'dark' ? 'bg-gray-2' : 'bg-white'}
                 `}>
                 <i className={`cil-moon text-[30px] ${$theme !== 'dark' ? 'text-gray-2' : 'text-gray-1'}`}></i>
             </div>
             <div className={`
-                flex justify-center items-center p-1 rounded-full 
+                flex justify-center items-center p-1 rounded-full transition-1
                 ${$theme !== 'light' ? 'bg-gray-2' : 'bg-white'}
                 `}>
                 <i className={`cil-sun text-[30px] ${$theme !== 'light' ? 'text-gray-2' : 'text-yellow-500'}`}></i>
