ff41effa04fea9419cf01c456a9b86bcd4f4f5b3
theman49 <cakdeny49@gmail.com>
2023-10-12 22:55:02 +0700

    update project list page

--- src/store.js
+++ src/store.js
@@ -10,6 +10,11 @@ const toggleTheme = () => {
 
     const body = document.querySelector('body')
     body.className = change === 'dark' ? 'bg-dark-1' : 'bg-light-1'
+
+    const popupElements = document.querySelectorAll('.popup')
+    popupElements.forEach(element => {
+        element.style.backgroundColor = change === 'dark' ? '#03001C' : '#5B8FB9'
+    });
 }
 
 export {
